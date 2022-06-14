fc77f776bc2e4a433262c0349c458ef57aa479bb
kwakdo <vv05044@gmail.com>
2022-06-14 15:26:57 +0900

    container

---
+++ container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
@@ -0,0 +1,42 @@
+package com.douzone.container.user.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import com.douzone.container.user.User1;
+
+public class XmlConfigTest {
+
+	public static void main(String[] args) {
+		// XML Auto Configuration(Annotation Scanning)
+		// testBeanFactory01();
+		
+		// XML Bean Configuration(Explicit Configuration)
+		// testBeanFactory02();
+	}
+
+	// XML Auto Configuration(Annotation Scanning)
+	private static void testBeanFactory01() {
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/douzone/container/config/user/applicationContext01.xml"));
+		
+		User1 user1 = bf.getBean(User1.class);
+		System.out.println(User1.getName());
+		
+		// Bean ID가 자동으로 설정됨
+		user1 = (User1)bf.getBean("user1");
+		System.out.println(User1.getName());
+	}
+	
+	// XML Bean Configuration(Explicit Configuration)
+	private static void testBeanFactory02() {
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/douzone/container/config/user/applicationContext02.xml"));
+			
+		User1 user1 = bf.getBean(User1.class);
+		System.out.println(User1.getName());
+			
+		// Bean ID가 자동으로 설정 안됨
+		user1 = (User1)bf.getBean("user1");
+		System.out.println(User1.getName());
+	}
+}
